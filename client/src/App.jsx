08180e81ca6cdb159ff237c8a1dc26977eec3045
Rohit Kumar <rk2613026@gmail.com>
2024-04-29 16:56:13 +0530

    Install React js with vite and create templates, pages & routes

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+import Posts from './pages/Posts';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/posts' element={<Posts />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
