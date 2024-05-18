71393a7c790346390661d485d24b467f1a7ffb4f
Rohit Kumar <rk2613026@gmail.com>
2024-05-18 11:59:20 +0530

    add create a post api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js'
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log("MongoDB is connected");
@@ -21,6 +23,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
